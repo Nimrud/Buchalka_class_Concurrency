e3e5c966404d81a537025cfe3de196261d8a7023
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-04-07 00:57:11 +0200

    m11_challenge - challenge 3 & 4

--- src/m11_challenge1/BankAccountReentrantLock.java
+++ src/m11_challenge1/BankAccountReentrantLock.java
@@ -1,7 +1,15 @@
 package m11_challenge1;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
+// Challenge 3:
+// Make BankAccount class threadsafe using ReentrantLock
+
+// Challenge 4:
+// Instead of using lock(), use tryLock() with a timeout value of 1 second.
+// If the waiting period times out, print "Could not get the lock" to the console
 
 public class BankAccountReentrantLock {
     private double balance;
@@ -15,15 +23,24 @@ public class BankAccountReentrantLock {
         // tworzymy tylko 1 locka, aby upewnić się, że tylko 1 operacja będzie wyk. w danej chwili
     }
 
-    public void deposit(double amount){
-        lock.lock();
-        try{
-            balance += amount;
-        } finally {
-            lock.unlock();
+    // Challenge 4:
+    public void deposit(double amount) {
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+                try {
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
         }
     }
 
+    // Challenge 3:
     public void withdraw(double amount){
         lock.lock();
         try {
